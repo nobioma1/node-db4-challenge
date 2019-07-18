979ff177a810a0719ffe560b5aa28acfa1a0384f
Noble Obioma <nobioma1@gmail.com>
2019-07-18 16:25:57 +0100

    Create Query builder to get ingredients and quantity

--- api/recipes/recipeModels.js
+++ api/recipes/recipeModels.js
@@ -1,7 +1,14 @@
 const db = require('../../data/dbConfig');
 
-module.exports = { getRecipes };
+module.exports = { getRecipes, getShoppingList };
 
 function getRecipes() {
   return db('recipes');
+}
+
+function getShoppingList(recipe_id) {
+  return db('recipeDetails')
+    .select('ingredients.ingredient_name', 'recipeDetails.ingredient_qty')
+    .innerJoin('ingredients', 'ingredient_id', 'ingredients.id')
+    .where({ recipe_id: recipe_id });
 }
