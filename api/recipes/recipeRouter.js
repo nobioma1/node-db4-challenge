2b61805dcbbf4eb0761aab6309dcc633855725a7
Noble Obioma <nobioma1@gmail.com>
2019-07-18 17:08:22 +0100

    Create API endpoint to get recipe tied to an ingredient

--- api/recipes/recipeRouter.js
+++ api/recipes/recipeRouter.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { getRecipes, getShoppingList, getInstructions } = require('./recipeModels');
+const {
+  getRecipes,
+  getShoppingList,
+  getInstructions,
+  getRecipesForIngredient,
+} = require('./recipeModels');
 
 const recipeRouter = express.Router();
 
@@ -14,6 +19,7 @@ recipeRouter.get('/', async (req, res) => {
 
 recipeRouter.get('/:id/shoppingList', async (req, res) => {
   try {
+    // id - recipeId
     const shoppingList = await getShoppingList(req.params.id);
     res.status(200).json(shoppingList);
   } catch (error) {
@@ -23,10 +29,21 @@ recipeRouter.get('/:id/shoppingList', async (req, res) => {
 
 recipeRouter.get('/:id/instructions', async (req, res) => {
   try {
+    // id - recipeId
     const instructions = await getInstructions(req.params.id);
     res.status(200).json(instructions);
   } catch (error) {
     res.status(500).json({ error: 'Error getting instructions for Recipe' });
+  }
+});
+
+recipeRouter.get('/:id/recipes', async (req, res) => {
+  try {
+    // id - ingredientId
+    const recipesForIngredient = await getRecipesForIngredient(req.params.id);
+    res.status(200).json(recipesForIngredient);
+  } catch (error) {
+    res.status(500).json({ error: 'Error getting recipes for ingredient' });
   }
 });
 
