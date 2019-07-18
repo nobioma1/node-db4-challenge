a00d9613293cd52028e86c6156b55b988554e18a
Noble Obioma <nobioma1@gmail.com>
2019-07-18 16:44:40 +0100

    Create query builder to get Instruction orderby by step

--- api/recipes/recipeModels.js
+++ api/recipes/recipeModels.js
@@ -1,6 +1,6 @@
 const db = require('../../data/dbConfig');
 
-module.exports = { getRecipes, getShoppingList };
+module.exports = { getRecipes, getShoppingList, getInstructions };
 
 function getRecipes() {
   return db('recipes');
@@ -10,5 +10,12 @@ function getShoppingList(recipe_id) {
   return db('recipeDetails')
     .select('ingredients.ingredient_name', 'recipeDetails.ingredient_qty')
     .innerJoin('ingredients', 'ingredient_id', 'ingredients.id')
-    .where({ recipe_id: recipe_id });
+    .where({ recipe_id });
+}
+
+function getInstructions(recipe_id) {
+  return db('instructions')
+    .select('instruction', 'instruction_step')
+    .where({ recipe_id })
+    .orderBy('instruction_step');
 }
