03b7d4797cc612d7d290e29c4de39c0d7bff03e4
Noble Obioma <nobioma1@gmail.com>
2019-07-18 16:02:30 +0100

    Create Server Route

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const recipeRouter = require('./recipes/recipeRouter');
 
 const server = express();
 server.use(express.json());
+server.use('/api/recipes/', recipeRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Node Recipe' });
